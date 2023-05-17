6604902110465e2e0352e7ba9f0e0f43433e686f
Nikita Churganov <74280461+nikitachurganov@users.noreply.github.com>
2023-05-17 22:23:33 +1000

    tugarin

--- Ilya_Godot_3/Scripts/Tugarin.gd
+++ Ilya_Godot_3/Scripts/Tugarin.gd
@@ -3,7 +3,7 @@ extends KinematicBody2D
 const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
 
 export var acceleration = 300
-export var max_speed = 50
+export var max_speed = 30
 export var friction = 1000
 
 enum{
@@ -41,19 +41,39 @@ func _physics_process(delta):
 				input_vector.y = global_position.y - player.global_position.y
 				animationTree.set("parameters/Idle/blend_position", input_vector)
 				animationTree.set("parameters/Run/blend_position", input_vector)
+				animationTree.set("parameters/Attack/blend_position", input_vector)
+				
 				animationState.travel("Run")
 				var direction = (player.global_position - global_position).normalized()
 				velocity = velocity.move_toward(direction * max_speed, acceleration * delta)
+				
+				
+				if  (player.global_position.x - global_position.x <=  1 && player.global_position.x - global_position.x >=  -1) ||(player.global_position.y - global_position.y <=  1 && player.global_position.y - global_position.y >=  -1) :
+					print(player.global_position - global_position)
+					state = ATTACK
+					
+				
 			else:
 				animationState.travel("Idle")
 				state = IDLE
+		ATTACK:
+			attack_state(delta)
+			
 	velocity = move_and_slide(velocity)
 
 func seek_player():
 	if playerDetectionZone.can_see_player():
 		state = CHASE
 
+func attack_state(delta):
+	velocity = Vector2.ZERO
+	animationState.travel("Attack")
+
+func attack_animation_finished():
+	state = CHASE
+
 func _on_Hurtbox_area_entered(area):
+	
 	stats.health -= PlayerStats.atk
 	knockback = area.knockback_vector * 40
 	hurtbox.create_hit_effect()
@@ -63,3 +83,10 @@ func _on_Stats_no_health():
 	var enemyDeathEffect = EnemyDeathEffect.instance()
 	get_parent().add_child(enemyDeathEffect)
 	enemyDeathEffect.global_position = global_position
+
+func _on_Hitbox_body_entered(body):
+	pass
+
+func _on_Hitbox_area_entered(area):
+	pass
+	
