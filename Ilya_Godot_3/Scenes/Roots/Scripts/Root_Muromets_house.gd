bc5f937e9d98827201922e8f946a6cefebafef14
donsko1 <donskoi.ya@student.dvfu.ru>
2023-05-24 11:55:33 +1000

    Root_Muromets_house

---
+++ Ilya_Godot_3/Scenes/Roots/Scripts/Root_Muromets_house.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+func _ready():
+	$SceenTransition/CollisionShape2D.disabled = true
+	connect_player_to_death()
+	equip_quest()
+	PlayerStats.connect("first_quest", self, "equip_quest")
+
+func save():
+	var data = {
+		"filename": get_filename()
+	}
+	
+	return data
+
+func connect_player_to_death():
+	var player = $world/YSort/Player
+	player.connect("on_death", $HealthUI/Control, "set_death_screen", [])
+
+func equip_quest():
+	if PlayerStats.sword != "" and PlayerStats.armor != "" and PlayerStats.helmet != "" and PlayerStats.quests[0] == "Надеть экипировку":
+		$SceenTransition/CollisionShape2D.disabled = false
+		PlayerStats.quests.pop_front()
+		$HealthUI/Control/Quest.quest_update()
