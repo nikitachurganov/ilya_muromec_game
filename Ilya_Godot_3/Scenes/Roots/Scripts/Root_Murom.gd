01dbda35a4bffd346b1e5a3629e643331f1af42e
donsko1 <donskoi.ya@student.dvfu.ru>
2023-05-25 02:53:59 +1000

    123

---
+++ Ilya_Godot_3/Scenes/Roots/Scripts/Root_Murom.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+func _ready():
+	$SceenTransition/CollisionShape2D.disabled = true
+	connect_player_to_death()
+	equip_quest()
+	PlayerStats.connect("first_quest", self, "equip_quest")
+
+func save():
+	var data = {
+		"filename": get_filename()
+	}
+	
+	return data
+
+func connect_player_to_death():
+	var player = $World/YSort/Player
+	player.connect("on_death", $HealthUI/Control, "set_death_screen", [])
+
+func equip_quest():
+	if PlayerStats.quests[0] == "Выйти из дома":
+		$SceenTransition/CollisionShape2D.disabled = false
+		PlayerStats.quests.pop_front()
+		$HealthUI/Control/Quest.quest_update()
