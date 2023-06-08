7c77aab22123cc799770348871c234e16d13bccb
donsko1 <donskoi.ya@student.dvfu.ru>
2023-06-08 16:52:54 +1000

    12345

--- Ilya_Godot_3/Scripts/MasterSlider.gd
+++ Ilya_Godot_3/Scripts/MasterSlider.gd
@@ -1,10 +1,13 @@
 extends HSlider
 
+signal slider_value_changed(value)
+
 func _ready():
 	# Подписка на глобальный сигнал в обеих сценах
-	get_tree().get_root().connect("slider_value_changed", self, "_on_slider_value_changed")
+	#get_tree().get_root().connect("slider_value_changed", self, "_on_slider_value_changed")
+	connect("slider_value_changed", self, "_on_slider_value_changed")
 
-	
+
 func _on_MasterSlider_value_changed(value):
 	emit_signal("slider_value_changed", value)
 	AudioServer.set_bus_volume_db(0, value)
